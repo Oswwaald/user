430dddfd42c8f721c34f91e89e5d08fa22a58d9d
Oswwaald <43680214+Oswwaald@users.noreply.github.com>
2018-10-01 18:39:26 +0200

    Update ServerInterface.java

--- ServerInterface.java
+++ ServerInterface.java
@@ -3,11 +3,15 @@ package ca.polymtl.inf8480.tp1.shared;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface ServerInterface extends Server {
-	String create(String fileName, String login, String password) throws RemoteException;
+/*
+ * Permet de construire l'interface accédant aux méthodes partagées par le Serveur.
+ * On retrouve ici les 6 méthodes précisées dans la spécification du partage de fichier.
+ */
+public interface ServerInterface extends Remote {
+	void create(String fileName, String login, String password) throws RemoteException;
 	byte[] get(String fileName, String checksumClient, String login, String password) throws RemoteException;
-	String push(String fileName, byte[] content, String login, String password) throws RemoteException;
-	String lock(String fileName, String checksumClient, String login, String password) throws RemoteException;
+	void push(String fileName, byte[] content, String login, String password) throws RemoteException;
+	void lock(String fileName, String checksumClient, String login, String password) throws RemoteException;
 	ArrayList<String> list(String login, String password) throws RemoteException;
 	void syncLocalDirectory(String login, String password) throws RemoteException;
 }
