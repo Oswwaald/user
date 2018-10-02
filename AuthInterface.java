bb4fefe8c712db8f561b268f0f38db8ccb7d76ff
Oswwaald <43680214+Oswwaald@users.noreply.github.com>
2018-10-02 21:20:37 +0200

    Update AuthInterface.java

--- AuthInterface.java
+++ AuthInterface.java
@@ -1,13 +1,14 @@
 package ca.polymtl.inf8480.tp1.shared;
 
+import java.io.IOException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 /*
- * Permet de construire l'interface accédant aux méthodes partagées par le Serveur d'Authentification.
- * On retrouve ici les 2 méthodes précisées dans la spécification de l'authentification.
+ * Permet de construire l'interface accedant aux methodes partagees par le Serveur d'Authentification.
+ * On retrouve ici les 2 methodes precisees dans la specification de l'authentification.
  */
 public interface AuthInterface extends Remote {
 	void newUser(String login, String password) throws RemoteException;
-	boolean verify(String login, String password) throws RemoteException;
+	boolean verify(String login, String password) throws RemoteException, IOException;
 }
