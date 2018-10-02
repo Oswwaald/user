209c72e1bc9d4932e05bc0a18d76709ed4a3a029
Oswwaald <43680214+Oswwaald@users.noreply.github.com>
2018-10-02 20:46:02 +0200

    Update ServerInterface.java

--- ServerInterface.java
+++ ServerInterface.java
@@ -1,5 +1,6 @@
 package ca.polymtl.inf8480.tp1.shared;
 
+import java.io.IOException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
@@ -8,10 +9,10 @@ import java.rmi.RemoteException;
  * On retrouve ici les 6 méthodes précisées dans la spécification du partage de fichier.
  */
 public interface ServerInterface extends Remote {
-	void create(String fileName, String login, String password) throws RemoteException;
-	String get(String fileName, String checksumClient, String login, String password) throws RemoteException;
-	void push(String fileName, String content, String login, String password) throws RemoteException;
-	void lock(String fileName, String checksumClient, String login, String password) throws RemoteException;
-	String[] list(String login, String password) throws RemoteException;
-	String[] syncLocalDirectory(String login, String password) throws RemoteException;
+	void create(String fileName, String login, String password) throws RemoteException, IOException;
+	String get(String fileName, String checksumClient, String login, String password) throws RemoteException, IOException;
+	void push(String fileName, String content, String login, String password) throws RemoteException, IOException;
+	void lock(String fileName, String checksumClient, String login, String password) throws RemoteException, IOException;
+	void list(String login, String password) throws RemoteException, IOException;
+	String[] syncLocalDirectory(String login, String password) throws RemoteException, IOException;
 }
