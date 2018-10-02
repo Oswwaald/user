0d85f843f977c3b92eaf85ffd6ed0b6c586dc2e1
Oswwaald <43680214+Oswwaald@users.noreply.github.com>
2018-10-02 16:54:18 +0200

    Update Client.java
    Modifs sur le Client

--- Client.java
+++ Client.java
@@ -5,6 +5,7 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.ArrayList;
 
 import ca.polymtl.inf8480.tp1.shared.ServerInterface;
 import ca.polymtl.inf8480.tp1.shared.AuthInterface;
@@ -15,6 +16,7 @@ public class Client {
 	 * Ebauche d'idée permettant de transmettre les bons arguments aux bonnes fonctions du Client ?! A développer...
 	 */
 	private int i = 1;
+	private String methodeExec = null;
 	private String argument1 = null;
 	private String argument2 = null;
 	private String argument3 = null;
@@ -59,25 +61,25 @@ public class Client {
 	 * Permet de lancer les différentes exécutions de requêtes apportées par le Client.
 	 */
 	private void run() {
-		if (methodeExec == newUser) {
+		if (methodeExec == "newUser") {
 			newUser(argument1, argument2);
 		}
-		else if (methodeExec == create) {
+		else if (methodeExec == "create") {
 			create(argument1, argument2, argument3);
 		}
-		else if (methodeExec == get) {
+		else if (methodeExec == "get") {
 			get(argument1, argument2, argument3, argument4);
 		}
-		else if (methodeExec == push) {
+		else if (methodeExec == "push") {
 			push(argument1, argument2, argument3, argument4);
 		}
-		else if (methodeExec == lock) {
+		else if (methodeExec == "lock") {
 			lock(argument1, argument2, argument3, argument4);
 		}
-		else if (methodeExec == list) {
+		else if (methodeExec == "list") {
 			list(argument1, argument2);
 		}
-		else if (methodeExec == syncLocalDirectory) {
+		else if (methodeExec == "syncLocalDirectory") {
 			syncLocalDirectory(argument1, argument2);
 		}
 		else 
@@ -125,7 +127,7 @@ public class Client {
 	/*
 	 * Lancement de la requête newUser du Client avec le Serveur d'authentification.
 	 */
-	 private void newUser() {
+	 private void newUser(String login, String password) {
 		 System.out.println("Le client a bien lancé la requête.");
 		 distantAuthStub.newUser(login, password);
 		 System.out.println("Le Serveur a fini de transmettre la réponse");
@@ -136,16 +138,16 @@ public class Client {
 	 */	 
 	 private void create(String fileName, String login, String password) {
 		 System.out.println("Le client a lancé la requête.");
-		 distantServerStub.create(filename, login, password);
+		 distantServerStub.create(fileName, login, password);
 		 System.out.println("Le Serveur a fini de transmettre la réponse");
 	 }
 	 
 	/* 
 	 * Lancement de la requête newUser du Client avec le Serveur d'authentification.
 	 */	 
-	 private void String get(String fileName, String checksumClient, String login, String password) {
+	 private String get(String fileName, String checksumClient, String login, String password) {
 		 System.out.println("Le client a lancé la requête.");
-		 distantServerStub.get(filename, checksumClient, login, password);
+		 distantServerStub.get(fileName, checksumClient, login, password);
 		 System.out.println("Le Serveur a fini de transmettre la réponse");
 	 }
 	
@@ -172,7 +174,7 @@ public class Client {
 	 */
 	 private ArrayList<String> list(String login, String password) {
 		 System.out.println("Le client a lancé la requête.");
-		 distantServerStub.list(loin, password);
+		 distantServerStub.list(login, password);
 		 System.out.println("Le Serveur a fini de transmettre la réponse");
 	 }
 
