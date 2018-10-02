9b931e9fc802bbe7d67dd190bb6f803e0b109c37
Oswwaald <43680214+Oswwaald@users.noreply.github.com>
2018-10-02 21:05:04 +0200

    Update AuthInterface.java

--- AuthInterface.java
+++ AuthInterface.java
@@ -4,8 +4,8 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 /*
- * Permet de construire l'interface accédant aux méthodes partagées par le Serveur d'Authentification.
- * On retrouve ici les 2 méthodes précisées dans la spécification de l'authentification.
+ * Permet de construire l'interface accedant aux methodes partagees par le Serveur d'Authentification.
+ * On retrouve ici les 2 methodes precisees dans la specification de l'authentification.
  */
 public interface AuthInterface extends Remote {
 	void newUser(String login, String password) throws RemoteException;
